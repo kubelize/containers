045b0885cf768058ed6523fb82601f9e75d4fbea
Daniel Hendricken <daniel.hendricken@bedag.ch>
2024-04-06 17:57:46 +0200

    chore: improved conan-exiles-server container

---
+++ game-servers/conan-exiles/run_server.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+./steamcmd.sh +@sSteamCmdForcePlatformType windows +login anonymous +app_update 443030 +quit
+./steamcmd.sh +@sSteamCmdForcePlatformType windows +login anonymous +workshop_download_item 440900 2791028919
+export WINEARCH=win64
+export WINEPREFIX=/home/steam/.wine64
+xvfb-run --auto-servernum wine /home/steam/Steam/steamapps/common/'Conan Exiles Dedicated Server'/ConanSandboxServer.exe -log
